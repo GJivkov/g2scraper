b2b49153f5d6c9d03e533503b9fb7fa4bdaf9c7b
Georgi Georgiev <gjgeorgievtge@gmail.com>
2021-07-30 16:17:34 +0200

    Add ratings, website, description

--- webscraper.py
+++ webscraper.py
@@ -1,12 +1,12 @@
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 CHROMEDRIVER_PATH = "chromedriver.exe" 
 BASE_URL = "https://www.g2.com/products/trello/reviews"
 
-driver = webdriver.Chrome(CHROMEDRIVER_PATH)
-driver.implicitly_wait(5)
+driver = webdriver.Chrome(CHROMEDRIVER_PATH) 
+driver.implicitly_wait(10)
 
 driver.get(BASE_URL)
 page = driver.page_source
@@ -15,7 +15,22 @@ accept_cookies = driver.find_element_by_xpath('//*[@id="new_user_consent"]/input
 
 html = BeautifulSoup(page, 'html.parser')
 
-description = html.select_one('div.ws-pw p').text
+description = html.select_one('div[itemprop$="description"]').text
+website = html.select_one('a[itemprop$="url"]')['href']
 
-print(description)
+# TODO: remove out of 5 stars
+ratings = html.select_one('div[class$="text-center ai-c star-wrapper__desc__rating"]').text
 
+# TODO: remove , and reviews word
+number_of_reviews = html.select_one('li[class$="list--piped__li"]').text 
+
+
+product_seller_details = html.find_all()
+
+# TODO: Optional: alternatives and pricing
+
+
+
+print(website)
+print(ratings)
+print(number_of_reviews)
