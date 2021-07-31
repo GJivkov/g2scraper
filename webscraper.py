aad59869971198308baa1913f38ed720bf6d9e64
Georgi Georgiev <gjgeorgievtge@gmail.com>
2021-07-31 12:13:16 +0200

    Added OOP and fetched csv data

--- webscraper.py
+++ webscraper.py
@@ -1,40 +1,87 @@
-from bs4 import BeautifulSoup, NavigableString
+import pandas as pd
+import regex as re
+from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
-CHROMEDRIVER_PATH = "chromedriver.exe" 
-BASE_URL = "https://www.g2.com/products/trello/reviews"
+CHROMEDRIVER_PATH = "chromedriver.exe"
+QUERY_URL = "https://www.g2.com/search?utf8=%E2%9C%93&query="
 
-driver = webdriver.Chrome(CHROMEDRIVER_PATH) 
-driver.implicitly_wait(10)
+def get_company_url(company):
+    company = company.replace(" ", "+")
+    
+    full_url = QUERY_URL + company
 
-driver.get(BASE_URL)
-page = driver.page_source
+    driver = webdriver.Chrome(CHROMEDRIVER_PATH) 
+    driver.implicitly_wait(10)
 
-accept_cookies = driver.find_element_by_xpath('//*[@id="new_user_consent"]/input[6]').click()
+    driver.get(full_url)
+    page = driver.page_source
 
-html = BeautifulSoup(page, 'html.parser')
+    accept_cookies = driver.find_element_by_xpath('//*[@id="new_user_consent"]/input[6]').click()
 
-description = html.select_one('div[itemprop$="description"]').text
-website = html.select_one('a[itemprop$="url"]')['href']
-
-# TODO: remove out of 5 stars
-ratings = html.select_one('div[class$="text-center ai-c star-wrapper__desc__rating"]').text
-
-# TODO: remove , and reviews word
-number_of_reviews = html.select_one('li[class$="list--piped__li"]').text 
-
-details_list = html.find_all("div", class_ = 'ml-1')
-
-details_titles = [p.next.text for p in details_list] #To use as column title in Excel
-
-for p in details_list:
-    p.find("div", class_ = 'fw-semibold').decompose()
-
-details_values = [p.text for p in details_list]
+    html = BeautifulSoup(page, 'html.parser')
+     
+    product_name = html.select_one('div[class$="product-listing__product-name"]')
+    
+    if product_name is not None:
+        return (product_name.find('a')['href'])
+    else:
+        return ("Search yielded no results")
+    
+    driver.quit()
 
 
+df = pd.read_csv('data_scientist_intern_g2_scraper.csv', encoding='utf-8')
+
+df['NAME'].replace(r'\(.*?\)', '', regex=True, inplace=True)
+df['NAME'].replace(r"^ +| +$", r"", regex=True, inplace=True) 
+
+def get_company_data(company_url):
+    
+    driver = webdriver.Chrome(CHROMEDRIVER_PATH) 
+    driver.implicitly_wait(10)
+    
+    driver.get(company_url)
+    page = driver.page_source
+
+    accept_cookies = driver.find_element_by_xpath('//*[@id="new_user_consent"]/input[6]').click()
+
+    html = BeautifulSoup(page, 'html.parser')
+
+    description = html.select_one('div[itemprop$="description"]').text
+    website = html.select_one('a[itemprop$="url"]')['href']
+
+    # TODO: remove out of 5 stars
+    ratings = html.select_one('div[class$="text-center ai-c star-wrapper__desc__rating"]').text
+
+    # TODO: remove , and reviews word
+    number_of_reviews = html.select_one('li[class$="list--piped__li"]').text 
+
+    details_list = html.find_all("div", class_ = 'ml-1')
+
+    details_titles = [p.next.text for p in details_list] #To use as column title in Excel
+
+    for p in details_list:
+        p.find("div", class_ = 'fw-semibold').decompose()
+    
+    details_values = [p.text for p in details_list]
+
+    details = dict(zip(details_titles, details_values))
+
+    driver.quit()
+
+    return {'description': description,
+            'website': website,
+            'number_of_reviews': number_of_reviews,
+            'details': details}
+
+company_url = get_company_url("Trello")
+
+print(company_url)
+
+company_data = get_company_data(company_url)
+
+print(company_data)
 # TODO: Optional: alternatives and pricing
-
-driver.quit()
 
